e57ef27db8856424be47c34d05842f72190b0469
mgarber <manuel.garber@umassmed.edu>
2022-02-28 14:19:09 -0500

    Fixed license copyright

--- src/main/java/edu/umms/garberlab/slam/Hisat3nSlamSplitterWriter.java
+++ src/main/java/edu/umms/garberlab/slam/Hisat3nSlamSplitterWriter.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License
  *
- * Copyright (c) 2009 The Broad Institute
+ * Copyright (c) 2022 University of Massachusetts Medical School
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
